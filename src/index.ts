050cd6433c1344120f23618c63125daa1334ba1e
RJ <me@rjchow.com>
2020-10-27 23:34:24 +0800

    feat: added normal box-muller method

--- src/index.ts
+++ src/index.ts
@@ -32,3 +32,26 @@ export const weibull = (lambda: number, k: number) => {
 
   return lambda * (-Math.log(1 - U)) ** (1 / k);
 };
+
+enum NormalDistributionGeneratorType {
+  boxmuller = "box-muller",
+}
+
+export const normal = (
+  mean: number,
+  stdev: number,
+  method: NormalDistributionGeneratorType = NormalDistributionGeneratorType.boxmuller
+) => {
+  const rng = getRng();
+
+  if (method === NormalDistributionGeneratorType.boxmuller) {
+    let u1 = 0;
+    let u2 = 0;
+    while (u1 === 0) u1 = rng(); // U1 and U2 need to exclude 0
+    while (u2 === 0) u2 = rng();
+    const R = Math.sqrt(-2.0 * Math.log(u1));
+    const theta = 2.0 * Math.PI * u2;
+    return mean + R * Math.cos(theta) * stdev;
+  }
+  throw new Error("Invalid Normal Distribution Generator");
+};
