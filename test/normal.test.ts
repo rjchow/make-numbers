81f511538bd3790b559c9b12c2b2c98efe567ce6
rjchow <me@rjchow.com>
2020-11-22 09:40:26 +0800

    feat: added test for normal distn

---
+++ test/normal.test.ts
@@ -0,0 +1,21 @@
+import { mean, variance } from "jstat";
+import { normal, NormalDistributionGeneratorType } from "../src/normal";
+import { fillArray } from "../src/util/fillArray";
+
+describe("Normal Distribution", () => {
+  describe("Box-Muller method", () => {
+    test("mean should be mu", () => {
+      const mu = 2;
+      const stdev = 4;
+      const vals = fillArray(100000, () => normal(mu, stdev, NormalDistributionGeneratorType.boxmuller));
+      expect(mean(vals) - mu).toBeLessThanOrEqual(0.1);
+    });
+
+    test("variance should be s2", () => {
+      const mu = 2;
+      const stdev = 4;
+      const vals = fillArray(100000, () => normal(mu, stdev, NormalDistributionGeneratorType.boxmuller));
+      expect(variance(vals) - stdev ** 2).toBeLessThanOrEqual(0.1);
+    });
+  });
+});
